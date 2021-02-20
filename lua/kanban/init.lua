56b02c50c9d86e3d7f8ea76a21108f32df00c6c7
Alex <alexandersimcock@gmail.com>
2021-02-20 22:58:49 +0000

    line breaks added

--- lua/kanban/init.lua
+++ lua/kanban/init.lua
@@ -1,15 +1,21 @@
+local win, bufh
+
 local function createFloatingWindow()
+    bufh = vim.api.nvim_create_buf(false, true)
+    vim.api.nvim_buf_set_option(bufh, 'bufhidden', 'wipe')
+
     local stats = vim.api.nvim_list_uis()[1]
     local width = stats.width
     local height = stats.height
 
-    local dim_height = math.ceil(width*0.8)
-    local dim_width = math.ceil(width*0.8 - 4)
+    local dim_height = math.ceil(height*0.8 - 4)
+    local dim_width = math.ceil(width*0.8)
 
     local row = math.ceil((height - dim_height) / 2 - 1)
-    local col = math.ceil((width - dim_width))
+    local col = math.ceil((width - (dim_width*1.1)))
 
-    local bufh = vim.api.nvim_create_buf(false, true)
+    local lb = math.ceil(dim_width/4) - 1
+
     local bufh_opts = {
         style='minimal',
         relative="editor",
@@ -30,16 +36,16 @@ local function createFloatingWindow()
 
     local border_buf = vim.api.nvim_create_buf(false, true)
     local border_lines = { '╔' .. string.rep('═', dim_width) .. '╗' }
-    local middle_line = '║' .. string.rep(' ', dim_width) .. '║'
+    local linebreak = '║' .. string.rep(' ', lb) .. '|' .. string.rep(' ', lb) .. '|' .. string.rep(' ', lb) .. '|' .. string.rep(' ', lb) .. '║'
     for i=1, dim_height do
-          table.insert(border_lines, middle_line)
+          table.insert(border_lines, linebreak)
     end
 
     table.insert(border_lines, '╚' .. string.rep('═', dim_width) .. '╝')
     vim.api.nvim_buf_set_lines(border_buf, 0, -1, false, border_lines)
 
     local border_win = vim.api.nvim_open_win(border_buf, true, border_opts)
-    vim.api.nvim_open_win(bufh, true, bufh_opts)
+    --vim.api.nvim_open_win(bufh, true, bufh_opts)
     vim.api.nvim_command('au BufWipeout <buffer> exe "silent bwipeout! "'..border_buf)
 end
 
