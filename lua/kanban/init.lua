fa1afb2d2f69c8843048fc60ee470326fb36f7ab
Alex <alexandersimcock@gmail.com>
2021-02-24 18:23:26 +0000

    Initial reading of columns

--- lua/kanban/init.lua
+++ lua/kanban/init.lua
@@ -14,77 +14,86 @@ end
 
 function fileChecker()
     local f=io.open('.todo.md', 'r')
-    if f~=nil then io.close(f) return true else return false end
+    local file_contents = f:read("*a")
+    local titles = {}
+
+    local file= io.open('.todo.md', 'r')
+    titles[0] = file:read()
+    local columns = #titles
+
+    return columns, titles
 end
 
 
 local function createFloatingWindow()
-  buf = vim.api.nvim_create_buf(false, true)
-  local border_buf = vim.api.nvim_create_buf(false, true)
+    buf = vim.api.nvim_create_buf(false, true)
+    local border_buf = vim.api.nvim_create_buf(false, true)
 
-  print(fileChecker())
+    local cols, titles = fileChecker()
+    print(cols)
+    print(titles[0])
 
-  vim.api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
-  vim.api.nvim_buf_set_option(buf, 'filetype', 'whid')
+    vim.api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
+    vim.api.nvim_buf_set_option(buf, 'filetype', 'whid')
 
-  local width = vim.api.nvim_get_option("columns")
-  local height = vim.api.nvim_get_option("lines")
+    local width = vim.api.nvim_get_option("columns")
+    local height = vim.api.nvim_get_option("lines")
 
-  local win_height = math.ceil(height * 0.8 - 4)
-  local win_width = math.ceil(width * 0.8)
-  local row = math.ceil((height - win_height) / 2 - 1)
-  local col = math.ceil((width - win_width) / 2)
+    local win_height = math.ceil(height * 0.8 - 4)
+    local win_width = math.ceil(width * 0.8)
+    local row = math.ceil((height - win_height) / 2 - 1)
+    local col = math.ceil((width - win_width) / 2)
 
-  local border_opts = {
-    style = "minimal",
-    relative = "editor",
-    width = win_width + 2,
-    height = win_height + 2,
-    row = row - 1,
-    col = col - 1
-  }
+    local border_opts = {
+        style = "minimal",
+        relative = "editor",
+        width = win_width + 2,
+        height = win_height + 2,
+        row = row - 1,
+        col = col - 1
+    }
 
-  local opts = {
-    style = "minimal",
-    relative = "editor",
-    width = win_width,
-    height = win_height,
-    row = row,
-    col = col
-  }
+    local opts = {
+        style = "minimal",
+        relative = "editor",
+        width = win_width,
+        height = win_height,
+        row = row,
+        col = col
+    }
 
-  local lb = math.floor(win_width/3)-3
-  local border_lines = { '╔' .. string.rep('═', win_width) .. '╗' }
-  local middle_line = '║' .. string.rep(' ', win_width) .. '║'
-  for i=1, win_height do
-    table.insert(border_lines, middle_line)
-  end
-  table.insert(border_lines, '╚' .. string.rep('═', win_width) .. '╝')
-  vim.api.nvim_buf_set_lines(border_buf, 0, -1, false, border_lines)
+    local lb = math.floor(win_width/3)-3
+    local border_lines = { '╔' .. string.rep('═', win_width) .. '╗' }
+    local middle_line = '║' .. string.rep(' ', win_width) .. '║'
+    for i=1, win_height do
+        table.insert(border_lines, middle_line)
+    end
+    table.insert(border_lines, '╚' .. string.rep('═', win_width) .. '╝')
+    vim.api.nvim_buf_set_lines(border_buf, 0, -1, false, border_lines)
 
-  local border_win = vim.api.nvim_open_win(border_buf, true, border_opts)
-  win = api.nvim_open_win(buf, true, opts)
-  api.nvim_command('au BufWipeout <buffer> exe "silent bwipeout! "'..border_buf)
+    local border_win = vim.api.nvim_open_win(border_buf, true, border_opts)
+    win = api.nvim_open_win(buf, true, opts)
+    api.nvim_command('au BufWipeout <buffer> exe "silent bwipeout! "'..border_buf)
 
-  vim.api.nvim_win_set_option(win, 'cursorline', true)
+    vim.api.nvim_win_set_option(win, 'cursorline', true)
 
-  api.nvim_buf_set_lines(buf, 0, -1, false, { center('Vim-Kanban'), '', ''})
-  api.nvim_buf_set_lines(buf, 1, -1, false, { center('TODO'), '', ''})
-  api.nvim_buf_add_highlight(buf, -1, 'WhidHeader', 0, 0, -1)
+    api.nvim_buf_set_lines(buf, 0, -1, false, { center('Vim-Kanban'), '', ''})
+    api.nvim_buf_set_lines(buf, 1, -1, false, { center('TODO'), '', ''})
+    api.nvim_buf_add_highlight(buf, -1, 'WhidHeader', 0, 0, -1)
 end
 
 
-local function onResize()
-    local stats = vim.api.nvim_list_uis()[1]
-    local width = stats.width
-    local height = stats.height
+    local function onResize()
+        local stats = vim.api.nvim_list_uis()[1]
+        local width = stats.width
+        local height = stats.height
 
-    print(width, height)
-    return width, height
-end
+        print(width, height)
+        return width, height
+    end
 
 
-return {
-    createFloatingWindow = createFloatingWindow,
-    onResize = onResize
-}
+    return {
+        createFloatingWindow = createFloatingWindow,
+        onResize = onResize
+    }
